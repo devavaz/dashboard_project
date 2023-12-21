4fbab392f85ee76c9aeef4b79c1ea2aca9143c3a
devavaz <135300758+devavaz@users.noreply.github.com>
2023-12-22 02:16:01 +0900

    Create DataTable.tsx

---
+++ src/pages/components/dataTable/DataTable.tsx
@@ -0,0 +1,35 @@
+import "./dataTable.scss"
+export default function DataTable() {
+    
+  return (
+    <div className="dataTable">
+          <div className="dataTable">
+      <DataGrid
+        className="dataGrid"
+        rows={props.rows}
+        columns={[...props.columns, actionColumn]}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 10,
+            },
+          },
+        }}
+        slots={{ toolbar: GridToolbar }}
+        slotProps={{
+          toolbar: {
+            showQuickFilter: true,
+            quickFilterProps: { debounceMs: 500 },
+          },
+        }}
+        pageSizeOptions={[5]}
+        checkboxSelection
+        disableRowSelectionOnClick
+        disableColumnFilter
+        disableDensitySelector
+        disableColumnSelector
+      />
+    </div>
+    </div>
+  )
+}
